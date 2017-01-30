157df73c7f4813976becb279ab56da5e958f5a6d
Kristoffer Lundgren <krlun@tyler2.local>
2017-01-30 18:56:49 +0100

    struktur

--- inlup1/handin1.m
+++ inlup1/handin1.m
@@ -1,7 +1,11 @@
-% Script for testing the tableau.
+% Handin1 FMA240
+
+%% Problem 2
+
 A = [1 1 1 0 0; 1 0 0 1 0; 8 20 0 0 1];
 b = [1;3/4;10];
 c = [2; 1; 0; 0; 0];
+
 basicvars = [3 4 5];
 [tableau, x, basic, feasible, optimal] = checkbasic1(A, b, c, basicvars);
 disp('Problem 2a');
@@ -17,10 +21,12 @@ basicvars = [3 1 2];
 disp('Problem 2c');
 tableau
 
-% Script for testing the tableau.
+%% Problem 3
+
 A = [3 2 1 0 0; 5 1 1 1 0; 2 5 1 0 1];
 b = [1; 3; 4];
-c = [-1; -1; -1; -1; -1]; %(converting min-problem to max-problem)
+c = [-1; -1; -1; -1; -1];
+
 basicvars = [3 4 5];
 [tableau, x, basic, feasible, optimal] = checkbasic1(A, b, c, basicvars);
 disp('Problem 3a');
@@ -30,3 +36,6 @@ basicvars = [2 4 5];
 [tableau, x, basic, feasible, optimal] = checkbasic1(A, b, c, basicvars);
 disp('Problem 3b');
 tableau
+
+%% Problem 4
+
