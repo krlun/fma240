329f4e00c14f51dd47af431dce63a2622b1d8ba6
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-07 14:51:59 +0100

    skeleton for boundy.m

---
+++ handin2/boundy.m
@@ -0,0 +1,9 @@
+function bounds=boundy(x,D,minmax);
+% function bounds=boundy(x,D,minmax);
+% calculates the 1x2 vector with lower and upper bound
+% respectively,
+% given the 1xn vector with the current path,
+% the NxN distance matrix D and the Nx2 matrix
+% minmax, where minmax(i,1) is the minimum distance
+% from city i and minmax(i,2) is the maximum distance
+% from city i.
