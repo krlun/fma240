5cb703da79248d6b4b2f303ea215f92686f92536
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-07 14:49:25 +0100

    branchy.m klar

---
+++ handin2/branchy.m
@@ -0,0 +1,17 @@
+function X=branchy(x,N);
+% function X=branchy(x,N);
+% returns the mx(n+1) matrix X where
+% each row of X is a possible extension
+% of the input path x.
+% x is a 1xn vector, and N is the total number
+% of cities in the problem.
+    
+    X = zeros(N-length(x), length(x)+1);
+
+    xtemp = 1:N;
+    xtemp(x) = [];
+    for i = 1:length(xtemp)
+        X(i, :) = [x xtemp(i)];
+    end
+    
+end
