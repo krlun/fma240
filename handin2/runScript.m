e339cefa79da3edcc4f06485d5055ea748c1cdf3
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-14 15:12:35 +0100

    multiple samples

--- handin2/runScript.m
+++ handin2/runScript.m
@@ -1,12 +1,17 @@
 NStart = 4;
-NEnd = 15;
+NEnd = 12;
 distanceLimit = 200;
+nrOfSamples = 20;
 time = [];
 for i = NStart:NEnd
-    tic
-    D = createDistanceMatrix(i, distanceLimit);
-    [x,fopt] = travsalesman(D)
-    time = [time toc];
+    sample = 0;
+    for j = 1:nrOfSamples
+        tic;
+        D = createDistanceMatrix(i, distanceLimit);
+        [x,fopt] = travsalesman(D)
+        sample = sample + toc;
+    end
+    time = [time sample/nrOfSamples];
 end
 subplot(2, 1, 1);
 plot(NStart:NEnd, time);
