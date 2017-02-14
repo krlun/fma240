c8063d1ab63989f0be554d77ec87230d30fdb118
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2017-02-14 16:26:22 +0100

    fixar

--- handin2/boundy.m
+++ handin2/boundy.m
@@ -1,22 +1,22 @@
-function bounds=boundy(path,D,dist_minmax);
-% function bounds=boundy(path,D,dist_minmax);
+function bounds=boundy(x,D,minmax);
+% function bounds=boundy(x,D,minmax);
 % calculates the 1x2 vector with lower and upper bound
 % respectively,
 % given the 1xn vector with the current path,
 % the NxN distance matrix D and the Nx2 matrix
-% dist_minmax, where dist_minmax(i,1) is the minimum distance
-% from city i and dist_minmax(i,2) is the maximum distance
+% dist_minmax, where minmax(i,1) is the minimum distance
+% from city i and minmax(i,2) is the maximum distance
 % from city i.
 
-    % Get the number of columns in D.
+    % Get the number of cities.
     [~, N] = size(D);
 
-    % RemainingCities on one line, slower because of setdiff?
+    % RemainingCities on one line, slower because of setdiff
 %    RemainingCities = setdiff(1:N, path);
 
     % Enumerate cities, unset cities already in path.
     RemainingCities = 1:N;
-    RemainingCities(path) = [];
+    RemainingCities(x) = [];
 
 %    distanceTraveled = 0;
 %    for i = 2:length(path)
@@ -30,10 +30,10 @@ function bounds=boundy(path,D,dist_minmax);
 
     bounds = remaining_minmax + current_distance;
 
-    if (length(path) == N),
+    if (length(x) == N),
         % Visited all cities, add return length.
-        bounds = bounds + D(path(end), path(1));
+        bounds = bounds + D(x(end), x(1));
     else % Haven't reached the end, add minmax for last visit.
-        bounds = bounds + dist_minmax(path(end), :);
+        bounds = bounds + dist_minmax(x(end), :);
     end
 end
--- handin2/branchy.m
+++ handin2/branchy.m
@@ -1,25 +1,25 @@
-function X=branchy(curr_path,N);
-% function X=branchy(curr_path,N);
+function X=branchy(x,N);
+% function X=branchy(x,N);
 % returns the mx(n+1) matrix X where
 % each row of X is a possible extension
-% of the input path curr_path.
-% path is a 1xn vector, and N is the total number
+% of the input path x.
+% x is a 1xn vector, and N is the total number
 % of cities in the problem.
 
      % Enumerate all cities 1 -> N, unset visited cities.
      remainingCities = 1:N;
-     remainingCities(curr_path) = [];
+     remainingCities(x) = [];
 
      % Set variables for row repetition.
-     num_remaining = N-length(curr_path);
+     num_remaining = N-length(x);
      rep_col = 1; % Constrain repetition to one column.
 
-     % Repeat current path num_remaining times.
-     X = [repmat(curr_path, num_remaining, rep_col)];
+     % Repeat current path num_remaining times, append remaining cities.
+     X = [repmat(x, num_remaining, rep_col)];
      % Append remaining cities.
      X = [X remainingCities'];
 
-%    oneliner, men långsam på grund av setdiff.
+%    one liner, slow because of setdiff
 %    X = [repmat(x, N-length(x), 1) setdiff(1:N, x)'];
 
 end
